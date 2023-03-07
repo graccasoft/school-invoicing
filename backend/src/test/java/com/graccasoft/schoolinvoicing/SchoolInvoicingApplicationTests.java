25b2495ffd168cb2d8eb3817d2756da06abf6f2d
Gracious Mashasha <graccasoft263@gmail.com>
2023-03-07 13:22:09 +0200

    Initialized spring boot maven project

---
+++ backend/src/test/java/com/graccasoft/schoolinvoicing/SchoolInvoicingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graccasoft.schoolinvoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolInvoicingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
