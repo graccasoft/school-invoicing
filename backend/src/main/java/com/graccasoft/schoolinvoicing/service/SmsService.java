7c0c5ca58d195bdf030f817470b5b8073df17a4c
Gracious Mashasha <graccasoft263@gmail.com>
2023-03-14 14:49:52 +0200

    Send SMS on payment receipt

--- backend/src/main/java/com/graccasoft/schoolinvoicing/service/SmsService.java
+++ backend/src/main/java/com/graccasoft/schoolinvoicing/service/SmsService.java
@@ -1,5 +1,8 @@
 package com.graccasoft.schoolinvoicing.service;
 
+import com.graccasoft.schoolinvoicing.model.Payment;
+
 public interface SmsService {
     void sendSMS(String phoneNumber, String content);
+    void sendPaymentSms(Payment payment);
 }
--- backend/src/main/java/com/graccasoft/schoolinvoicing/service/impl/SmsServiceImpl.java
+++ backend/src/main/java/com/graccasoft/schoolinvoicing/service/impl/SmsServiceImpl.java
@@ -1,11 +1,18 @@
 package com.graccasoft.schoolinvoicing.service.impl;
 
+import com.graccasoft.schoolinvoicing.model.Payment;
+import com.graccasoft.schoolinvoicing.model.SystemOption;
 import com.graccasoft.schoolinvoicing.service.SmsService;
+import com.graccasoft.schoolinvoicing.service.SystemOptionService;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class SmsServiceImpl implements SmsService {
@@ -19,6 +26,12 @@ public class SmsServiceImpl implements SmsService {
     @Value("${twilio-sender-number}")
     private String twilioSender;
 
+    private final SystemOptionService systemOptionService;
+
+    public SmsServiceImpl(SystemOptionService systemOptionService) {
+        this.systemOptionService = systemOptionService;
+    }
+
     @Override
     public void sendSMS(String phoneNumber, String content)  throws RuntimeException {
         Twilio.init(twilioAccountSID, twilioAuthToken);
@@ -30,5 +43,18 @@ public class SmsServiceImpl implements SmsService {
         if (message.getStatus() == Message.Status.FAILED ){
             throw new RuntimeException("SMS Not sent");
         }
+    }
+
+    @Override
+    public void sendPaymentSms(Payment payment) {
+        String message = "Payment of " + payment.getAmount() + " received for " + payment.getStudent();
+        SystemOption smsReceivers = systemOptionService.getSystemOption("PAYMENT_SMS_RECEIVERS");
+        if(smsReceivers == null)
+            return;
+
+        List<String> phoneNumbers = Arrays.asList( smsReceivers.getValue().split(",") );
+        phoneNumbers.forEach(phoneNumber->{
+            sendSMS(phoneNumber,message);
+        });
     }
 }
