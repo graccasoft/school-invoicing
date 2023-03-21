341cf46a577084f934f3227ffc98b3eba1386d04
Gracious Mashasha <graccasoft263@gmail.com>
2023-03-21 11:40:58 +0200

    Update backend/src/main/java/com/graccasoft/schoolinvoicing/controller/InvoiceController.java
    Co-authored-by: graccasoft263 <gracious.mashasha@gmail.com>

--- backend/src/main/java/com/graccasoft/schoolinvoicing/controller/InvoiceController.java
+++ backend/src/main/java/com/graccasoft/schoolinvoicing/controller/InvoiceController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("invoice")
+@RequestMapping("invoices")
 public class InvoiceController {
 
     private final InvoiceService invoiceService;
